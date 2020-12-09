1a479b85b0bc28e5c33e9953d05ef9548c78604d
Denis Belotserkovets <belocerkovecden@gmail.com>
2020-12-09 12:50:10 +0300

    Вывод из конфига

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,10 @@ class HomeController extends Controller
 {
 
     public function index () {
+        dump($_ENV['MY_SETTING']);
+        dump(config('app.timezone'));
+        dump(config('database.connections.mysql.database'));
+        dump($_ENV);
         return view('home', ['var' => 6545666]);
     }
 
